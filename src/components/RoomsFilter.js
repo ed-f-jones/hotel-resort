881215844c040ad3d937e145c89948714ee4c78a
unknown <ed.f.jones@gmail.com>
2020-05-10 18:54:26 -0400

    completed the rooms layout

---
+++ src/components/RoomsFilter.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {useContext} from "react";
+import {RoomContext} from "../context";
+import Title from "../components/Title";
+
+export default function RoomsFilter() {
+    return (
+        <div>
+            hello from rooms filter
+        </div>
+    )
+}
--- src/pages/Rooms.js
+++ src/pages/Rooms.js
@@ -2,9 +2,12 @@ import React from 'react'
 import Hero from "../components/Hero";
 import Banner from "../components/Banner";
 import {Link} from "react-router-dom";
+import RoomContainer from "../components/RoomContainer";
+
 
 export default function Rooms() {
     return (
+        <>
         <Hero hero="roomsHero">
         <Banner title="our rooms">
             <Link to="/" className="btn-primary">
@@ -12,5 +15,7 @@ export default function Rooms() {
             </Link>
         </Banner>
         </Hero>
+        <RoomContainer/>
+        </>
     );
 }
